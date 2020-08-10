6a8a7875463cf48f8f7207dfa5dedc18e9aceb0e
hepengfei <Pfhe@censoft.com.cn>
2020-08-10 18:26:27 +0800

    算法复习_第二节复习分治问题2.0 双向开始排查数据

--- src/class03forExit/Code03_PartitionAndQuickSort.java
+++ src/class03forExit/Code03_PartitionAndQuickSort.java
@@ -2,8 +2,12 @@ package class03forExit;
 
 import util.utilMethod;
 
-public class Code03_PartitionAndQuickSort {
 
+
+public class Code03_PartitionAndQuickSort {
+    /**
+     * partition分治问题，左边比某个值小，右边比某个值大
+     * */
     public static int partition(int[] arr,int L, int R){
         if(arr==null || L > R) return -1;
 
@@ -20,6 +24,20 @@ public class Code03_PartitionAndQuickSort {
         return lessEqual;
     }
 
+    //在数组的给定范围之内，查找以最后一个数字为轴，小于等于排前面，大于排后面。最后将轴放到中心，返回轴位置
+    public static  int  partition2(int[] arr, int leftPor,int rightPor) {
+        int pivot = arr[rightPor];
+        int left = leftPor;
+        int right = rightPor - 1;
+        while (left <= right) {
+            while (left <= right && arr[left] <= pivot) left++; //寻找左边大于pivot 的第一个值
+            while (left <= right && arr[right] > pivot) right--;//寻找右边小于pivot 的第一个值
+            if (left < right) utilMethod.swap(arr, left, right);//交换
+        }
+        //交换最左边和最后一个数
+        utilMethod.swap(arr, left, rightPor);
+        return left;
+    }
 
 
     public static void main(String[] args) {
